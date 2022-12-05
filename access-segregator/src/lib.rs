f98f4e8d401fed88a02b8b326115be59c1f35e91
tolak <kalot.wang@gmail.com>
2022-12-05 20:59:25 +0800

    extrinsic identification & test

--- access-segregator/src/lib.rs
+++ access-segregator/src/lib.rs
@@ -3,7 +3,7 @@
 pub use self::pallet::*;
 
 // #[cfg(test)]
-// mod mock;
+mod mock;
 
 #[allow(unused_variables)]
 #[allow(clippy::large_enum_variant)]
@@ -17,7 +17,7 @@ pub mod pallet {
 	/// Mapping signature of extrinsic to account has access
 	#[pallet::storage]
 	#[pallet::getter(fn extrinsic_access)]
-	pub type ExtrinsicAccess<T: Config> = StorageMap<_, Twox64Concat, [u8; 4], T::AccountId>;
+	pub type ExtrinsicAccess<T: Config> = StorageMap<_, Twox64Concat, (u8, u32), T::AccountId>;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub (super) trait Store)]
@@ -31,20 +31,25 @@ pub mod pallet {
 
 		/// Origin used to administer the pallet
 		type BridgeCommitteeOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+
+		/// Current pallet index defined in runtime
+		type PalletIndex: Get<u8>;
 	}
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub (super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Extrinsic access grant to someone
-		/// args: [sig, who]
-		AccessGranted { sig: [u8; 4], who: T::AccountId },
+		/// args: [pallet_index, extrinsic_index, who]
+		AccessGranted { pallet_index: u8, extrinsic_index: u32, who: T::AccountId },
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		/// Function unimplemented
 		Unimplemented,
+		/// Failed to grant extrinsic access permission to an account
+		GrantAccessFailed,
 	}
 
 	#[pallet::call]
@@ -53,24 +58,85 @@ pub mod pallet {
 		#[pallet::weight(195_000_000)]
 		pub fn grant_access(
 			origin: OriginFor<T>,
-			sig: [u8; 4],
+			pallet_index: u8,
+			extrinsic_index: u32,
 			who: T::AccountId,
 		) -> DispatchResult {
-			// Ensure bridge committee
-			T::BridgeCommitteeOrigin::ensure_origin(origin)?;
+			// Ensure bridge committee or the account that has permisson to grant access to an
+			// extrinsic
+			if T::BridgeCommitteeOrigin::ensure_origin(origin.clone()).is_err() {
+				let who = ensure_signed(origin)?;
+				// 0 is extrinsc index of `grant_access` by default
+				let extrinsic_index = frame_system::Pallet::<T>::extrinsic_index().unwrap_or(0);
+				ensure!(
+					ExtrinsicAccess::<T>::get((T::PalletIndex::get(), extrinsic_index)).is_some(),
+					Error::<T>::GrantAccessFailed
+				);
+			}
 
 			// Apply access
-			ExtrinsicAccess::<T>::insert(&sig, &who);
+			ExtrinsicAccess::<T>::insert((pallet_index, extrinsic_index), &who);
 
 			// Emit AccessGranted event
-			Self::deposit_event(Event::AccessGranted { sig, who });
+			Self::deposit_event(Event::AccessGranted { pallet_index, extrinsic_index, who });
 			Ok(())
 		}
 	}
 
 	impl<T: Config> Pallet<T> {
-		pub fn has_access(sig: [u8; 4], caller: T::AccountId) -> bool {
-			ExtrinsicAccess::<T>::get(&sig).map_or(false, |who| who == caller)
+		pub fn has_access(pallet_index: u8, extrinsic_index: u32, caller: T::AccountId) -> bool {
+			ExtrinsicAccess::<T>::get((pallet_index, extrinsic_index))
+				.map_or(false, |who| who == caller)
+		}
+	}
+
+	#[cfg(test)]
+	mod test {
+		use crate as access_segregator;
+		use crate::{
+			mock::{
+				assert_events, new_test_ext, AccessSegregator, PalletIndex, RuntimeEvent as Event,
+				RuntimeOrigin as Origin, Test, ALICE, BOB,
+			},
+			Event as AccessSegregatorEvent,
+		};
+		use frame_support::{assert_noop, assert_ok};
+
+		#[test]
+		fn should_work() {
+			new_test_ext().execute_with(|| {
+				// (PalletIndex:get(), 0) indicates extrinsic: `grant_access` of this pallet
+				assert_noop!(
+					AccessSegregator::grant_access(Some(ALICE).into(), PalletIndex::get(), 0, BOB),
+					access_segregator::Error::<Test>::GrantAccessFailed
+				);
+				assert!(!AccessSegregator::has_access(PalletIndex::get(), 0, ALICE));
+				assert_ok!(AccessSegregator::grant_access(
+					Origin::root(),
+					PalletIndex::get(),
+					0,
+					ALICE
+				));
+				assert!(AccessSegregator::has_access(PalletIndex::get(), 0, ALICE));
+
+				// ALICE grants access permission to BOB for an extrinsic (100, 100)
+				assert_ok!(AccessSegregator::grant_access(Some(ALICE).into(), 100, 100, BOB));
+				assert!(!AccessSegregator::has_access(100, 100, ALICE));
+				assert!(AccessSegregator::has_access(100, 100, BOB));
+
+				assert_events(vec![
+					Event::AccessSegregator(AccessSegregatorEvent::AccessGranted {
+						pallet_index: PalletIndex::get(),
+						extrinsic_index: 0,
+						who: ALICE,
+					}),
+					Event::AccessSegregator(AccessSegregatorEvent::AccessGranted {
+						pallet_index: 100,
+						extrinsic_index: 100,
+						who: BOB,
+					}),
+				]);
+			})
 		}
 	}
 }
---
+++ access-segregator/src/mock.rs
@@ -0,0 +1,117 @@
+#![cfg(test)]
+
+use frame_support::{
+	pallet_prelude::ConstU32,
+	parameter_types,
+	sp_runtime::{
+		testing::{Header, H256},
+		traits::{BlakeTwo256, IdentityLookup},
+		AccountId32, Perbill,
+	},
+};
+use frame_system::{self as system, EnsureRoot};
+
+use crate as access_segregator;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+pub(crate) type Balance = u128;
+
+pub const ALICE: AccountId32 = AccountId32::new([0u8; 32]);
+pub const BOB: AccountId32 = AccountId32::new([1u8; 32]);
+
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		AccessSegregator: access_segregator::{Pallet, Call, Storage, Event<T>} = 2,
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const MaxLocks: u32 = 100;
+	pub const MinimumPeriod: u64 = 1;
+}
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId32;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = BlockHashCount;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<2>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: Balance = 1;
+}
+
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type RuntimeEvent = RuntimeEvent;
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+}
+
+parameter_types! {
+	// Make sure put same value with `construct_runtime`
+	pub const PalletIndex: u8 = 2;
+}
+
+impl access_segregator::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type BridgeCommitteeOrigin = EnsureRoot<Self::AccountId>;
+	type PalletIndex = PalletIndex;
+}
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
+}
+
+// Checks events against the latest. A contiguous set of events must be provided. They must
+// include the most recent event, but do not have to include every past event.
+pub fn assert_events(mut expected: Vec<RuntimeEvent>) {
+	let mut actual: Vec<RuntimeEvent> =
+		system::Pallet::<Test>::events().iter().map(|e| e.event.clone()).collect();
+
+	expected.reverse();
+
+	for evt in expected {
+		let next = actual.pop().expect("event expected");
+		assert_eq!(next, evt, "Events don't match");
+	}
+}
