0e9f05ea11658e0883c53fd16a4677cf750025db
Freddyli7 <tobeacookie@gmail.com>
2023-02-15 15:05:57 -0500

    add script for proposal execution test

---
+++ scripts/js/execute_proposal_test.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+
+const {ApiPromise, WsProvider, Keyring} = require('@polkadot/api');
+const {cryptoWaitReady} = require('@polkadot/util-crypto');
+
+const proposal = {
+    origin_domain_id: 1,
+    deposit_nonce: 0,
+    resource_id: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+    data: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 243, 16, 122, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 37, 0, 1, 1, 0, 128, 212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125])
+}
+// signer should be the same MPC address as setup.js
+const signature = new Uint8Array([105, 77, 128, 179, 208, 211, 79, 162, 226, 176, 224, 35, 189, 103, 173, 194, 57, 210, 254, 56, 57, 107, 223, 35, 190, 123, 124, 130, 181, 1, 200, 195, 60, 133, 109, 135, 244, 182, 13, 129, 199, 18, 35, 34, 66, 157, 102, 159, 49, 10, 222, 178, 121, 204, 169, 23, 134, 48, 46, 10, 234, 189, 71, 14, 27]);
+
+async function executeProposal(api, proposalList, signature, finalization, sudo) {
+    return new Promise(async (resolve, reject) => {
+        const nonce = Number((await api.query.system.account(sudo.address)).nonce);
+
+        console.log(
+            `--- Submitting extrinsic to execute dummy proposal for testing ---`
+        );
+        const unsub = await api.tx.sudo
+            .sudo(api.tx.sygmaBridge.executeProposal(proposalList, signature))
+            .signAndSend(sudo, {nonce: nonce, era: 0}, (result) => {
+                console.log(`Current status is ${result.status}`);
+                if (result.status.isInBlock) {
+                    console.log(
+                        `Transaction included at blockHash ${result.status.asInBlock}`
+                    );
+                    if (finalization) {
+                        console.log('Waiting for finalization...');
+                    } else {
+                        unsub();
+                        resolve();
+                    }
+                } else if (result.status.isFinalized) {
+                    console.log(
+                        `Transaction finalized at blockHash ${result.status.asFinalized}`
+                    );
+                    unsub();
+                    resolve();
+                } else if (result.isError) {
+                    console.log(`Transaction Error`);
+                    reject(`Transaction Error`);
+                }
+            });
+    });
+}
+
+async function queryAssetBalance(api, assetID, account) {
+    let result = await api.query.assets.account(assetID, account);
+    return result.toHuman()
+}
+
+async function main() {
+    const sygmaPalletProvider = new WsProvider(process.env.PALLETWSENDPOINT || 'ws://127.0.0.1:9944');
+    const api = await ApiPromise.create({
+        provider: sygmaPalletProvider,
+    });
+
+    await cryptoWaitReady();
+    const keyring = new Keyring({type: 'sr25519'});
+    const sudo = keyring.addFromUri('//Alice');
+
+    const balanceBefore = await queryAssetBalance(2000, sudo.address)
+    console.log('asset balance info before: ', balanceBefore);
+
+    await executeProposal(api, [proposal], signature, true, sudo);
+
+    const balanceAfter = await queryAssetBalance(2000, sudo.address)
+    console.log('asset balance info after: ', balanceAfter);
+}
+
+main().catch(console.error).finally(() => process.exit());
