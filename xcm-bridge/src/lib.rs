da30fb971335d2ab3cc4bb806bd1abd519385fb8
Freddyli7 <tobeacookie@gmail.com>
2023-11-28 12:12:57 -0500

    add xcm message struct and execution logic

--- xcm-bridge/src/lib.rs
+++ xcm-bridge/src/lib.rs
@@ -15,12 +15,57 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        type Weigher: WeightBounds<Self::RuntimeCall>;
+
+        type XcmExecutor: ExecuteXcm<Self::RuntimeCall>;
     }
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         XCMTransferSend {},
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        FailToWeightMessage,
+        XcmExecutionFailed,
+    }
+
+    struct Xcm<T: Config>{
+        asset: MultiAsset,
+        origin: MultiLocation,
+        dest: MultiLocation,
+        recipient: MultiLocation,
+        weight: XCMWeight,
+    }
+
+    pub trait XcmHandler {
+        fn create_message(&self) -> Result<Xcm<T::RuntimeCall>, DispatchError>;
+        fn execute_message(&self, xcm_message: Xcm<T::RuntimeCall>) -> DispatchResult;
+    }
+
+    impl XcmHandler for Xcm {
+        fn create_message(&self) {
+
+        }
+
+        fn execute_message(&self, xcm_message: Xcm<T::RuntimeCall>) {
+            let message_weight = T::Weigher::weight(xcm_message).map_err(|()| Error::<T>::FailToWeightMessage)?;
+
+            let hash = xcm_message.using_encoded(sp_io::hashing::blake2_256);
+
+            T::XcmExecutor::execute_xcm_in_credit(
+                self.origin.clone(),
+                xcm_message.clone(),
+                hash,
+                message_weight,
+                message_weight
+            ).ensure_complete().map_err(|_| Error::<T>::XcmExecutionFailed)?;
+
+            oK(())
+        }
     }
 
     #[pallet::call]
